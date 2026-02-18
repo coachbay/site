ef11880e42a051f98f7fa79c25c52c4b6949bd25
coachbay <coach@coachbay.com>
2026-02-19 00:39:40 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,6 +1,12 @@
 import './index.css'
 import AIDiagnostic from './Diagnostic'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App() {
-  return <AIDiagnostic />
+  return (
+    <>
+      <AIDiagnostic />
+      <Analytics />
+    </>
+  )
 }
